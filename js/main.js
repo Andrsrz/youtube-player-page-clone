9e14adba621e62932af4d2642a6aa129cf69c8d8
Andres Ruiz <andrsruiz@gmail.com>
2020-05-26 16:38:44 -0500

    Add Dynamic Divs

--- js/main.js
+++ js/main.js
@@ -1,10 +1,6 @@
-const RE_1 = [];
-const RE_2 = [];
-const RE_3 = [];
-const RE_4 = [];
-const RE_5 = [];
-
-const RECOMENDATIONS_ARR = [RE_1, RE_2, RE_3, RE_4, RE_5];
+const RE_1 = new Recomendation("AZKABAN - WITCHCRAFT AND WIZARDRY - Directo 3", "alexelcapo 76.010 visualizaciones Hace 4 d&iacute;as");
+const RE_2 = new Recomendation("MINECRAFT DUNGEONS - Juego completo", "alexelcapo 274.085 visualizaciones Hace 3 d&iacute;as");
+const RECOMENDATIONS_ARR = [RE_1, RE_2];
 
 function createRecomendations(arr){
 	let container = document.getElementById("site-main-video-recomendations");
@@ -14,9 +10,18 @@ function createRecomendations(arr){
 		divRecomendation.setAttribute("class", "recomendation");
 		let divLeft = document.createElement("div");
 		divLeft.setAttribute("class", "recomendation-video");
+		let icon = document.createElement("i");
+		icon.setAttribute("class", "fas fa-image thumbnail");
+		divLeft.appendChild(icon);
 		divRecomendation.appendChild(divLeft);
 		let divRigth = document.createElement("div");
 		divRigth.setAttribute("class", "recomendation-info");
+		let title = document.createElement("h2");
+		title.innerHTML = arr[i].title;
+		let des = document.createElement("h4");
+		des.innerHTML = arr[i].des;
+		divRigth.appendChild(title);
+		divRigth.appendChild(des);
 		divRecomendation.appendChild(divRigth);
 		container.appendChild(divRecomendation);
 	}
---
+++ js/recomendation.js
@@ -0,0 +1,6 @@
+class Recomendation {
+	constructor(title, des){
+		this.title = title;
+		this.des = des;
+	}
+};
